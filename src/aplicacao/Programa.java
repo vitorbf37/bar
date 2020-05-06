9b944a4feb68e5a1a895595c9f6162f67d50baef
Vitor Braga <vitorbf37@gmail.com>
2020-05-06 20:25:49 -0300

    Relatorio gerado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -23,6 +23,57 @@ public class Programa {
 		ref = sc.nextInt();
 		System.out.println("Quantidade de espetinhos: "); 
 		esp = sc.nextInt();
+
+		if(sexo=='F'){
+			total = (cer*5.0) + (esp*7.0) + (ref*3.0) + 8.0;
+			System.out.println("Relatorio: ");
+			System.out.printf("Consumo= %.2f%n", (cer*5.0) + (esp*7.0) + (ref*3.0) );
+			
+			if(total<=38) {
+				total = total+4;
+				System.out.println("Couvert: R$ 4.00");
+			}
+			else {
+				System.out.println("Isento de Covert");
+				
+			}
+			
+			System.out.println("Ingresso: R$ 8.00");
+			
+			System.out.printf("Valor a pagar= R$ %.2f", total );
+			
+			
+	}
+		
+		
+		if(sexo=='M'){
+			
+			total = (cer*5.0) + (esp*7.0) + (ref*3.0) + 10.0;
+			System.out.println("Relatorio: ");
+			System.out.printf("Consumo= %.2f%n", (cer*5.0) + (esp*7.0) + (ref*3.0) );
+			
+			if(total<=40) {
+				total = total+4;
+				System.out.println("Couvert: R$ 4.00");
+			}
+			else {
+				System.out.println("Isento de Covert");
+				
+			}
+			
+			System.out.println("Ingresso: R$ 10.00");
+			
+			System.out.printf("Valor a pagar= R$ %.2f", total );
+			
+			
+	}
+			
+			
+	
+			
+		
+		 
+		
 		
 		sc.close();
 	}
